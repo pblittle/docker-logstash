889f43a467e6a1af068cd5b91d59d4079651dc47
P. Barrett Little <barrett@barrettlittle.com>
2014-02-04 23:24:08 -0500

    Rewrite config with linked container env vars.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+sed -i "s/ES_HOST:${ES_PORT_9200_TCP_ADDR}/g" \
+    -i "s/ES_PORT:${ES_PORT_9200_TCP_PORT}/g" \
+    > /opt/logstash.conf
 
 exec java \
      -jar /opt/logstash.jar \
