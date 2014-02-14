e4b0e4ce8893181a02569aefc587f170c1fc939e
P. Barrett Little <barrett@barrettlittle.com>
2014-02-14 12:15:15 -0500

    Use the latest commit rather than a specific revision.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ LOGSTASH_CONFIG_FILE="/opt/logstash.conf"
 LOGSTASH_CONFIG_URL=${LOGSTASH_CONFIG_URL:-}
 
 if [ -z "${LOGSTASH_CONFIG_URL}" ]; then
-    LOGSTASH_CONFIG_URL="https://gist.github.com/pblittle/8778567/raw/b6ea950e17fbd2b657c850b11f34d1d754c327d2/logstash.conf"
+    LOGSTASH_CONFIG_URL="https://gist.github.com/pblittle/8778567/raw/logstash.conf"
 fi
 
 wget $LOGSTASH_CONFIG_URL -O $LOGSTASH_CONFIG_FILE
