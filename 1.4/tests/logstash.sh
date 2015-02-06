d78f2a20f1685810723995f230a6bb96b80e848f
P. Barrett Little <barrett@barrettlittle.com>
2015-02-06 14:31:16 -0500

    Cleaning up test commands. More to follow.

--- 1.4/tests/logstash.sh
+++ 1.4/tests/logstash.sh
@@ -12,34 +12,34 @@ function print_fail()
     echo "Fail: ${1}"
 }
 
-port_check=`netstat -an | grep ':9292\|:9200'`
+port_check=$(netstat -an | grep ':9292\|:9200')
 port_status=$?
 
 if [[ $port_status != 0 || $port_check = '' ]]; then
-    print_fail port_check
+    print_fail "$port_check"
 else
-    print_pass port_check
+    print_pass "$port_check"
 fi
 
-process_check=`ps -ef | grep 'logstash' | grep -v grep`
+process_check=$(pgrep -f logstash)
 process_status=$?
 
 if [[ $process_status != 0 || $process_check = '' ]]; then
-    print_fail process_check
+    print_fail "$process_check"
 else
-    print_pass process_check
+    print_pass "$process_check"
 fi
 
-curl_check=`curl -s -S localhost:9200/_nodes?pretty=true`
+curl_check=$(curl -s -S localhost:9200/_nodes?pretty=true)
 curl_status=$?
 
 if [[ $curl_status != 0 || $curl_check = '' ]]; then
-    print_fail curl_check
+    print_fail "$curl_check"
 else
-    print_pass curl_check
+    print_pass "$curl_check"
 fi
 
-test_kibana_es_protocol=`cat /opt/logstash/vendor/kibana/config.js | grep 'http'`
+test_kibana_es_protocol=$(grep 'http' /opt/logstash/vendor/kibana/config.js)
 
 if [[ $? != 0 || $? = '' ]]; then
     print_fail $?
@@ -47,7 +47,7 @@ else
     print_pass $?
 fi
 
-test_kibana_es_host=`cat /opt/logstash/vendor/kibana/config.js | grep '127.0.0.1'`
+test_kibana_es_host=$(grep '127.0.0.1' /opt/logstash/vendor/kibana/config.js)
 
 if [[ $? != 0 || $? = '' ]]; then
     print_fail $?
@@ -55,7 +55,7 @@ else
     print_pass $?
 fi
 
-test_kibana_es_port=`cat /opt/logstash/vendor/kibana/config.js | grep '9200'`
+test_kibana_es_port=$(grep '9200' /opt/logstash/vendor/kibana/config.js)
 
 if [[ $? != 0 || $? = '' ]]; then
     print_fail $?
@@ -63,7 +63,7 @@ else
     print_pass $?
 fi
 
-test_elasticsearch_config=`cat /opt/logstash/elasticsearch.yml | grep 'script.disable_dynamic: true'`
+test_elasticsearch_config=$(grep 'script.disable_dynamic: true' /app/elasticsearch.yml)
 
 if [[ $? != 0 || $? = '' ]]; then
     print_fail $?
